ff6c6bcdebbcc46c73d68887ba306f187c930dab
bLuDd1 <dtavluj@gmail.com>
2023-03-07 01:20:09 +0200

    added quadratic equation solver
    added function to solve quadratic equation

---
+++ solver.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const solveQuadratic = (a, b, c) => {
+    const d = Math.pow(b, 2) - 4 * a * c;
+
+    if (d > 0) {
+        const x1 = (-b + Math.sqrt(d)) / (2 * a);
+        const x2 = (-b - Math.sqrt(d)) / (2 * a);
+        console.log(`x1 = ${x1}\nx2 = ${x2}`);
+    } else if (d === 0) {
+        const x = -b / (2 * a);
+        console.log(`x = ${x}`);
+    } else {
+        console.log("There are no roots.")
+    }
+}
